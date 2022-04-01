9c678f4a65da035be4ee87ef2c8b077362c987f7
noman444 <87985544+noman444@users.noreply.github.com>
2022-04-02 03:24:03 +0500

    first commit

---
+++ static/js/15.993ed70c.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppancake-frontend"]=this["webpackJsonppancake-frontend"]||[]).push([[15],{1286:function(n,c){},1336:function(n,c){},1338:function(n,c){},1371:function(n,c){},1389:function(n,c){},1391:function(n,c){}}]);
+//# sourceMappingURL=15.993ed70c.chunk.js.map
